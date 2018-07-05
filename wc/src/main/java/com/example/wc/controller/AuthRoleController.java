2e3c407697e9d9489aaca2bb3ddfb0a16c2bfe7a
huangqiao <461187322@qq.com>
2018-07-05 17:55:22 +0800

    wxpay官方sdk，可单独作为jar包引用

--- wc/src/main/java/com/example/wc/controller/AuthRoleController.java
+++ wc/src/main/java/com/example/wc/controller/AuthRoleController.java
@@ -1,7 +1,9 @@
 package com.example.wc.controller;
 
+import com.example.wc.entity.result.WcFailure;
 import com.example.wc.model.AuthRole;
 import com.example.wc.service.AuthRoleService;
+import com.example.wc.utils.result.IResult;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("role")
 public class AuthRoleController {
 
     @Autowired
     private AuthRoleService authRoleService;
 
-    @RequestMapping(value = "/role", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public AuthRole findOneCity() {
         String roleId = "ROLE_ADMIN";
         return authRoleService.selectByPrimaryKey(roleId);
@@ -26,4 +29,8 @@ public class AuthRoleController {
         return authRoleService.selectAll(pageNum, pageSize);
     }
 
+    @RequestMapping(value = "/save", method = RequestMethod.GET)
+    public IResult save(){
+        return WcFailure.USER_SERVER_ERROR.setArgs(new Object[]{"报错了"});
+    }
 }
--- wc/src/main/java/com/example/wc/entity/Greeting.java
+++ wc/src/main/java/com/example/wc/entity/Greeting.java
@@ -1,7 +1,10 @@
 package com.example.wc.entity;
 
+import javax.validation.constraints.NotNull;
+
 public class Greeting {
 
+    @NotNull
     private String content;
 
     public Greeting() {
